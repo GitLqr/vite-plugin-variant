c0987b08b1bc492ea578be5affa252effca8d69e
GitLqr <linqiarui@gmail.com>
2022-10-23 21:59:24 +0800

    refact: vue3 demo 关联插件工程

--- examples/vite-vue3-demo/vite.config.ts
+++ examples/vite-vue3-demo/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vitePluginVariant from "vite-plugin-variant";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vitePluginVariant()],
 });
