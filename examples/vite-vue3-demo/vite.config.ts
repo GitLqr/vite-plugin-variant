ca1c28ddc7700e9c3a00a5496667121f19983105
GitLqr <linqiarui@gmail.com>
2022-10-28 02:52:23 +0800

    update: vite-vue3-demo config variant plugin

--- examples/vite-vue3-demo/vite.config.ts
+++ examples/vite-vue3-demo/vite.config.ts
@@ -4,5 +4,11 @@ import vitePluginVariant from "vite-plugin-variant";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vitePluginVariant()],
+  plugins: [
+    vue(),
+    vitePluginVariant({
+      mcsCurrent: process.env.CHANNEL,
+      debug: true,
+    }),
+  ],
 });
